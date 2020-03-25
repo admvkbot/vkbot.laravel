d4afb9e4fb671bbb2c124710be4dd158ec6c5891
admvkbot <sergerudy.mts@gmail.com>
2020-03-25 12:36:23 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/admin', function () {
     $vars = DB::table('accounts')->get();
     return view('admin', compact('vars'));
