b13bfe534ea553fbf45d7637c476582ec4f28692
admvkbot <sergerudy.mts@gmail.com>
2020-03-26 23:57:44 +0300

    overview

---
+++ resources/views/bot/admin/components/breadcrumb.blade.php
@@ -0,0 +1,12 @@
+<h1>
+    @if (isset($title)){{$title}}@endif
+</h1>
+<ol class="breadcrumb">
+    <li>
+        <a href=""><i class="fa fa-dashboard"></i>{{$parrent}}</a>
+    </li>
+    @if (isset($manage_own_accounts))
+        <li><a href=""><i></i>{{$manage_own_accounts}}</a></li>
+    @endif
+    <li><i class="active"></i>{{$active}}</li>
+</ol>
